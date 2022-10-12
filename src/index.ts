b8c637e8e4383e0e7968e9dab090eb5437fa39b9
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-10-12 19:05:31 -0400

    building

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,67 @@
+require("dotenv").config();
+import web3 from "web3";
 const EspaniconSDKNode = require("@espanicon/espanicon-sdk");
 const utils = require("./utils/utils");
-console.log(utils);
 
-const apiNode = "lisbon.net.solidwallet.io";
-const nid = 2;
-const lib = new EspaniconSDKNode(apiNode, nid);
-const { getScoreApi } = lib;
+// types
+interface Wallet {
+  address: string | undefined;
+  pk: string | undefined;
+}
+//
+const iconNode = {
+  node: "lisbon.net.solidwallet.io",
+  nid: 2
+};
+const bscNode = {
+  node: utils.networks.testnet.bsc.uri,
+  nid: utils.networks.testnet.bsc.network_id
+};
+
+const walletsRaw: { icon: Wallet; bsc: Wallet } = {
+  icon: {
+    address: process.env.WALLET_ICON_ADDRESS,
+    pk: process.env.WALLET_ICON_PK
+  },
+  bsc: {
+    address: process.env.WALLET_BSC_ADDRESS,
+    pk: process.env.WALLET_BSC_PK
+  }
+};
+
+const iconLib = new EspaniconSDKNode(iconNode.node, iconNode.nid);
+const bscLib = new web3(bscNode.node);
 
 async function runAsync() {
-  const query = await getScoreApi(utils.networks.testnet.icon.contracts.bts);
-  console.log("query");
-  console.log(JSON.stringify(query));
+  let bscQuery: any = null;
+  let iconQuery: any = null;
+
+  try {
+    iconQuery = await iconLib.getIcxBalance(walletsRaw.icon.address);
+    if (typeof walletsRaw.bsc.address === "string") {
+      bscQuery = await bscLib.eth.getBalance(
+        walletsRaw.bsc.address,
+        (err, wei) => {
+          if (err) {
+            console.log("error inside");
+            console.log(err);
+          }
+          const balance = web3.utils.fromWei(wei, "ether");
+          console.log("balance inside callback");
+          console.log(balance);
+          return balance;
+        }
+      );
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("error running queries");
+      console.log(err.message);
+    }
+    console.log("unexpected error", err);
+  }
+  console.log("bscQuery", bscQuery);
+  console.log("iconQuery", iconQuery);
 }
 
 runAsync();
