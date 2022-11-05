6c74f325b3455860d071953bcb77fbe1beb28b6a
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-11-04 23:14:00 -0400

    building sdk

--- src/tests/node/icon-bridge-sdk.test.ts
+++ src/tests/node/icon-bridge-sdk.test.ts
@@ -84,23 +84,180 @@ async function iconBridgeSDKTests() {
   //
   await testUtils.runTestOnMethod(sdkTestnet.bsc.methods.getAccumulatedFees);
 
-  // getNativeCoinName
-  // getOwners
-  // isOwner
-  // isValidCoin
-  // addOwner
-  // handleResponseService
-  // initialize
-  // mint
-  // reclaim
-  // refund
-  // register
-  // removeOwner
-  // setFeeRatio
-  // transfer
-  // transferBatch
-  // transferFees
-  // transferNativeCoin
+  //// Test 7: iconBridgeSDK.bsc.methods.getNativeCoinName()
+  //console.log(`Test 7: iconBridgeSDK.bsc.methods.getNativeCoinName()`);
+  ////
+  //await testUtils.runTestOnMethod(sdkTestnet.bsc.methods.getNativeCoinName);
+
+  //// Test 8: iconBridgeSDK.bsc.methods.getOwners()
+  //console.log(`Test 8: iconBridgeSDK.bsc.methods.getOwners()`);
+  ////
+  //await testUtils.runTestOnMethod(sdkTestnet.bsc.methods.getOwners);
+
+  //// Test 9: iconBridgeSDK.bsc.methods.isOwner(_owner)
+  //console.log(`Test 9: iconBridgeSDK.bsc.methods.isOwner(_isOwner: string)`);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.isOwner,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56"
+  //);
+
+  //// Test 10: iconBridgeSDK.bsc.methods.isValidCoin(_coinName)
+  //console.log(
+  //  `Test 10: iconBridgeSDK.bsc.methods.isValidCoin(_coinName: string)`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.isValidCoin,
+  //  "btp-0x2.icon-ICX"
+  //);
+
+  //// Test 11: iconBridgeSDK.bsc.methods.addOwner(_owner)
+  //console.log(`Test 11: iconBridgeSDK.bsc.methods.addOwner(_owner: string)`);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.addOwner,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56"
+  //);
+
+  //// Test 12: iconBridgeSDK.bsc.methods.handleResponseService(_requester, _coinName, _value, _fee, _rspCode)
+  //console.log(
+  //  `Test 12: iconBridgeSDK.bsc.methods.handleResponseService(_requester, _coinName, _value, _fee, _rspCode)`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.handleResponseService,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000000,
+  //  2000,
+  //  232
+  //);
+
+  //// Test 13: iconBridgeSDK.bsc.methods.initialize(_nativeCoinName, _feeNumerator, _fixedFee)
+  //console.log(
+  //  `Test 13: iconBridgeSDK.bsc.methods.initialize(_nativeCoinName, _feeNumerator,  _fixedFee)`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.initialize,
+  //  "btp-0x2.icon-ICX",
+  //  20000000000000,
+  //  2000
+  //);
+
+  //// Test 14: iconBridgeSDK.bsc.methods.mint(_to, _coinName, _value)
+  //console.log(
+  //  `Test 14: iconBridgeSDK.bsc.methods.mint(_to, _coinName, _value)`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.mint,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+
+  //// Test 15: iconBridgeSDK.bsc.methods.reclaim(
+  //console.log(
+  //  `Test 15: iconBridgeSDK.bsc.methods.reclaim(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.reclaim,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 16: iconBridgeSDK.bsc.methods.refund(
+  //console.log(
+  //  `Test 16: iconBridgeSDK.bsc.methods.refund(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.refund,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 17: iconBridgeSDK.bsc.methods.register(
+  //console.log(
+  //  `Test 17: iconBridgeSDK.bsc.methods.register(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.register,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 18: iconBridgeSDK.bsc.methods.removeOwner(
+  //console.log(
+  //  `Test 18: iconBridgeSDK.bsc.methods.removeOwner(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.removeOwner,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 19: iconBridgeSDK.bsc.methods.setFeeRatio(
+  //console.log(
+  //  `Test 19: iconBridgeSDK.bsc.methods.setFeeRatio(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.setFeeRatio,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 20: iconBridgeSDK.bsc.methods.transfer(
+  //console.log(
+  //  `Test 20: iconBridgeSDK.bsc.methods.transfer(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.transfer,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 21: iconBridgeSDK.bsc.methods.transferBatch(
+  //console.log(
+  //  `Test 21: iconBridgeSDK.bsc.transferBatch(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.transferBatch,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 22: iconBridgeSDK.bsc.methods.transferFees(
+  //console.log(
+  //  `Test 22: iconBridgeSDK.bsc.methods.transferFees(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.transferFees,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+  //// Test 23: iconBridgeSDK.bsc.methods.transferNativeCoin(
+  //console.log(
+  //  `Test 23: iconBridgeSDK.bsc.methods.transferNativeCoin(`
+  //);
+  ////
+  //await testUtils.runTestOnMethod(
+  //  sdkTestnet.bsc.methods.trasnferNativeCoin,
+  //  "0x4DeD312eB774B9828665448C55Faa8AE15353E56",
+  //  "btp-0x2.icon-ICX",
+  //  20000000000
+  //);
+
   //
   console.log("Test summary:");
   console.log(testSummary);
@@ -108,127 +265,3 @@ async function iconBridgeSDKTests() {
 }
 
 export = iconBridgeSDKTests;
-
-// // Test 1: fetch logic contract of a proxy contract (BTSCore) on the
-// // BSC Mainnet
-// console.log(mainBreak);
-// console.log(
-//   "Running test on sdk.bsc.methods.getLogicContractAddressOnChain()"
-// );
-// console.log(secondaryBreak);
-// const a = sdkMainnet.sdkUtils.contracts.bsc.mainnet.BTSCore.address;
-// const b = await sdkMainnet.bsc.methods.getLogicContractAddressOnChain(a);
-// console.log("testing mainnet");
-// console.log("proxy contract");
-// console.log(a);
-// console.log("logic contract");
-// console.log(b);
-// console.log(secondaryBreak);
-
-// // // Test 2: fetch logic contract of a proxy contract (BTSCore) on the
-// // // BSC Testnet
-// // const a1 = sdkTestnet.sdkUtils.contracts.bsc.testnet.BTSCore.address;
-// // const b1 = await sdkTestnet.bsc.methods.getLogicContractAddressOnChain(a1);
-// // console.log("testing testnet");
-// // console.log("proxy contract");
-// // console.log(a1);
-// // console.log("logic contract");
-// // console.log(b1);
-// // console.log(secondaryBreak);
-
-// // // Test 3: fetch abi of a contract
-// // // BSC Mainnet
-// // console.log(mainBreak)
-// // console.log("Running test on sdk.bsc.methods.getAbiOf");
-// // console.log(secondaryBreak)
-// // const a2 = "BTSCore";
-// // const b2 = sdkMainnet.bsc.methods.getAbiOf(a2);
-// // console.log("testing mainnet");
-// // console.log("contract label");
-// // console.log(a2);
-// // console.log("fetched abi:");
-// // console.log(JSON.stringify(b2));
-// // console.log(secondaryBreak)
-
-// // // Test 4: fetch abi of a contract
-// // // BSC Testnet
-// // const a3 = "BTSCore";
-// // const b3 = sdkTestnet.bsc.methods.getAbiOf(a3);
-// // console.log("testing Testnet");
-// // console.log("contract label");
-// // console.log(a3);
-// // console.log("fetched abi:");
-// // console.log(JSON.stringify(b3));
-// // console.log(secondaryBreak)
-
-// // // Test 5: get BTS Core proxy contract web3js object
-// // // BSC Mainnet
-// // console.log(mainBreak);
-// // console.log("Running test on sdk.bsc.methods.getBTSCoreProxyContractObject()");
-// // console.log(secondaryBreak);
-// // const b4 = await sdkMainnet.bsc.methods.getBTSCoreProxyContractObject();
-// // console.log("testing mainnet");
-// // console.log("BTSCore proxy contract");
-// // console.log(b4);
-// // console.log(secondaryBreak);
-
-// // // Test 6: get BTS Core logic contract web3js object
-// // // BSC Mainnet
-// // // TODO: This test will fail because the BTS Core logic contract abi
-// // // cannot be currently fetched from he bsc tracker
-// // console.log(mainBreak);
-// // console.log("Running test on sdk.bsc.methods.getBTSCoreLogicContractObject()");
-// // console.log(secondaryBreak);
-// // const b6 = await sdkMainnet.bsc.methods.getBTSCoreLogicContractObject();
-// // console.log("testing mainnet");
-// // console.log("BTSCore logic contract");
-// // console.log(b6);
-// // console.log(secondaryBreak);
-
-// // // Test 7: get BTS Core proxy contract web3js object
-// // // BSC testnet
-// // console.log(mainBreak);
-// // console.log("Running test on sdk.bsc.methods.getBTSCoreProxyContractObject()");
-// // console.log(secondaryBreak);
-// // const b7 = await sdkTestnet.bsc.methods.getBTSCoreProxyContractObject();
-// // console.log("testing testnet");
-// // console.log("BTSCore proxy contract");
-// // console.log(b7);
-// // console.log(secondaryBreak);
-
-// // // Test 8: get BTS Core logic contract web3js object
-// // // BSC Mainnet
-// // console.log(mainBreak);
-// // console.log("Running test on sdk.bsc.methods.getBTSCoreLogicContractObject()");
-// // console.log(secondaryBreak);
-// // const b8 = await sdkTestnet.bsc.methods.getBTSCoreLogicContractObject();
-// // console.log("testing testnet");
-// // console.log("BTSCore logic contract");
-// // console.log(b8);
-// // console.log(secondaryBreak);
-
-// // Test 9: get BTSCore.coinNames()
-// // BSC Mainnet
-// console.log(mainBreak);
-// console.log("Running test on sdk.bsc.methods.coinNames()");
-// console.log(secondaryBreak);
-// const b9 = await sdkTestnet.bsc.methods.coinNames();
-// console.log("testing testnet");
-// console.log("BTSCore.coinNames()");
-// console.log(b9);
-// console.log(secondaryBreak);
-
-// // Test 11: fetch BTS ABI
-// // BSC Testnet
-// const b11 = sdkTestnet.bsc.methods.getBTSAbi();
-// console.log("testing Testnet");
-// console.log("fetched abi:");
-// b11.forEach((each: any, index: number) => {
-//   if (each.type === "function") {
-//     console.log(`index: ${index}`);
-//     console.log(`name: ${each.name}`);
-//     console.log(`inputs: ${JSON.stringify(each.inputs)}`);
-//     console.log("##########");
-//   }
-// });
-// console.log(secondaryBreak);
