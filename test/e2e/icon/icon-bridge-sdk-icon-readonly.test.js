3090f9b13cf3250a2a9873131c6597b8cb00ab74
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-11-17 23:13:04 -0400

    building sdk: creating e2e tests

---
+++ test/e2e/icon/icon-bridge-sdk-icon-readonly.test.js
@@ -0,0 +1,62 @@
+// E2E Tests.
+require("dotenv").config();
+const IconBridgeSDK = require("../../../dist/icon-bridge-sdk-node");
+const assert = require("assert");
+
+// Icon Bridge SDK
+const sdk = new IconBridgeSDK({
+  useMainnet: false
+});
+
+// test wallets
+const wallets = {
+  icon: process.env.WALLET_ICON_ADDRESS,
+  bsc: process.env.WALLET_BSC_ADDRESS
+};
+
+// Keys that should appear in any response from a readonly call
+// originating from the ICON chain.
+const genericResultKeys = ["id", "jsonrpc", "result"];
+
+// ********************************************//
+// Test 1: calls 'balanceOf' method originating on the Icon chain. It should
+// returns and object with a predefined set of keys.
+describe("E2E testing Icon Bridge SDK. Chain: 'icon', Method 'balanceOf(_owner, _coinName)'", () => {
+  it("Returns BTP balance of an ICON wallet.", async () => {
+    const query = await sdk.icon.methods.balanceOf(
+      wallets.icon,
+      "btp-0x2.icon-bnUSD"
+    );
+    // check if the result query has the valid keys
+    const result = Object.keys(query).map(key => {
+      if (genericResultKeys.includes(key)) {
+        return true;
+      }
+      return false;
+    });
+
+    console.log(`\n  > Result: ${JSON.stringify(query)}\n`);
+    assert.ok(!result.includes(false));
+  }).timeout(5000);
+});
+
+// ********************************************//
+// Test 2: calls 'balanceOfBatch' method originating on the Icon chain.
+// It should returns and object with a predefined set of keys.
+describe("E2E testing Icon Bridge SDK. Chain: 'icon', Method 'balanceOfBatch(_owner, _coinNames)'", () => {
+  it("Returns BTP balance of an ICON wallet for a set of coins.", async () => {
+    const query = await sdk.icon.methods.balanceOfBatch(wallets.icon, [
+      "btp-0x2.icon-bnUSD"
+    ]);
+    // check if the result query has the valid keys
+    const result = Object.keys(query).map(key => {
+      if (genericResultKeys.includes(key)) {
+        return true;
+      }
+      return false;
+    });
+
+    console.log(`\n  > Result: ${JSON.stringify(query)}\n`);
+    assert.ok(!result.includes(false));
+  }).timeout(5000);
+});
