cc28690244f1e91db7a93ac82214358f9007352f
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-12-11 23:34:56 -0400

    building sdk: make build

--- dist/icon-bridge-sdk-web.mjs
+++ dist/icon-bridge-sdk-web.mjs
@@ -1,12 +1,15 @@
-import EspaniconSDKWeb from "@espanicon/espanicon-sdk";
+import EspaniconSDK from "@espanicon/espanicon-sdk";
 import IconBridgeSDK from "./lib/icon-bridge-sdk.js";
+import bscNodeBridge from "./lib/bsc/icon-bridge-sdk-node-bsc.js";
+import iconNodeBridge from './lib/icon/icon-bridge-sdk-node-icon.js';
 const defaultParams = {
     useMainnet: true
 };
 export default class IconBridgeSDKWeb extends IconBridgeSDK {
     constructor(inputParams = defaultParams) {
         super(inputParams);
-        this.IconWeb3 = new EspaniconSDKWeb(this.params.iconProvider.hostname, this.params.iconProvider.nid);
+        this.icon = new iconNodeBridge(this.params, this.sdkUtils, EspaniconSDK);
+        this.bsc = new bscNodeBridge(this.params, this.bscWeb3, this.sdkUtils, this.lib, this.icon.queryMethod);
     }
 }
 //# sourceMappingURL=icon-bridge-sdk-web.mjs.map
